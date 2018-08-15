472de3081705288c3e6b262167af7f9704f13427
Dhiraj <dhirajbhatt072@gmail.com>
2018-08-15 23:24:07 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Notes
+
+A very simple app to take notes.
+
+
+## Built With
+
+* [Node.js](https://nodejs.org/en/)
+
+## Authors
+
+* [Dhiraj](https://github.com/dhiraj072)
+
---
+++ app.js
@@ -0,0 +1,8 @@
+console.log('Starting app');
+
+const fs = require('fs');
+const os = require('os');
+const notes = require('./notes');
+
+const user = os.userInfo();
+fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);

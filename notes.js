2cf22b26521760cddb07730e4efd896f25b17ebe
Dhiraj <dhirajbhatt072@gmail.com>
2018-08-19 13:04:17 +0800

    Add functionality to add notes and save them to a local file

--- notes.js
+++ notes.js
@@ -1,6 +1,21 @@
+const fs = require('fs');
 
 const addNote = (title, body) => {
     console.log('Adding note', title, body);
+    let notes = [];
+    try {
+        notes = JSON.parse(fs.readFileSync('notes-data.json'));
+    } catch(e) {
+        console.log("Unable to load notes from file, using empty");
+    }
+    const duplicateNotes = notes.filter((note) => note.title === title);
+    if (duplicateNotes.length === 0) {
+        notes.push({
+            title,
+            body
+        });
+        fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+    }
 };
 
 const getAll = () => {
